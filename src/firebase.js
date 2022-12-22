120cebe225b448027ff6baaa6a0b9c1b10f9cacc
rajendra dhaka <rajendradhaka13@gmail.com>
2022-12-22 23:55:54 +0530

    configured firebase

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions we need from the SDKs we need
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+// Our web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBakoJujVQf9m5yYawkDOP8ARrRL7Bdpxg",
+  authDomain: "contact-book-b4962.firebaseapp.com",
+  projectId: "contact-book-b4962",
+  storageBucket: "contact-book-b4962.appspot.com",
+  messagingSenderId: "846935646333",
+  appId: "1:846935646333:web:3a5011e8e03c939a8f1114"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Storage and get a reference to the service
+export const storage = getStorage(app);
